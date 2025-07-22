b87e75d820283398cd2b988a1785f50c50072d9f
dinesh-heera <62005797+dinesh-heera@users.noreply.github.com>
2025-07-21 23:01:38 -0700

    Delete chat_interface.py

--- chat_interface.py
+++
@@ -1,8 +0,0 @@
-import chainlit as cl
-from engine.langgraph_engine import run_engine
-
-@cl.on_message
-async def on_message(message: cl.Message):
-    user_prompt = message.content
-    result = await run_engine(user_prompt)
-    await cl.Message(content=result).send()
